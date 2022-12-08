c83eee47ca58c1af138057746d29a943053cf84b
sohailabbas11 <sohailabbas669@gmail.com>
2022-12-08 16:47:34 +0100

    new pages

--- apps/client/src/Components/Home.tsx
+++ apps/client/src/Components/Home.tsx
@@ -6,18 +6,18 @@ const home = () => {
             <div className='flex flex-col px-8 py-8 gap-3 bg-slate-900 text-white h-screen w-72'>
                 <span className=" rounded-lg h-7 w-16 font-bold text-3xl text-center">Charity</span>
                 <div className='pt-10 space-y-4'>
-                    <span className="flex flex-row"> Home</span>
-                    <span className="flex flex-row"> Add new</span>
-                    <span className="flex flex-row" > Pending</span>
-                    <span className="flex flex-row">  Payed</span>
-                    <span className="flex flex-row"> Deleted Recently</span>
-                    <span className="flex flex-row"> Reminders</span>
-                    <span className="flex flex-row"> Notes</span>
-                    <span className="flex flex-row"> Reffered</span>
+                    <span className="flex flex-row "> Home</span>
+                    <span className="flex flex-row "> Add new</span>
+                    <span className="flex flex-row " > Pending</span>
+                    <span className="flex flex-row ">  Payed</span>
+                    <span className="flex flex-row "> Deleted Recently</span>
+                    <span className="flex flex-row "> Reminders</span>
+                    <span className="flex flex-row "> Notes</span>
+                    <span className="flex flex-row "> Reffered</span>
                     <div className="py-20">
                     </div>
                     <hr />
-                    <span>Help & Support</span>
+                    <span>Privacy & Settings</span>
                 </div>
             </div>
             <div className="w-screen bg-slate-700 ">

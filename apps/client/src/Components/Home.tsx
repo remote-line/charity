3203860daf12052a9f03401e347ac09a9e74c069
sohailabbas11 <sohailabbas669@gmail.com>
2022-12-08 11:36:59 +0100

    pages created with routes

--- apps/client/src/Components/Home.tsx
+++ apps/client/src/Components/Home.tsx
@@ -3,8 +3,8 @@ import '../index.css'
 
 const home = () => {
     return (
-        <div className='bg-cover' >
-            <img src={require('../Assets/sadqa.jpg')} alt="" />
+        <div className='bg-slate-400 h-screen p-10 flex justify-center' >
+            <h1 className='text-2xl font-black'>Welcome to Charity App</h1>
         </div>
     )
 }

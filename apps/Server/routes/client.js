d71815eda782562bb829f28ae3e4b8c3575fa868
ChNafees5 <chnafees54@gmail.com>
2022-12-09 19:01:06 +0500

    update

--- apps/Server/routes/client.js
+++ apps/Server/routes/client.js
@@ -1,16 +1,35 @@
 const router = require('express').Router()
 const User = require('../models/client')
-
-router.post('/register', async(req, res) => {
+router.post('/register', async (req, res) => {
     try {
-         const newUser = new User ({
-            username: req.body.name,
+        const newUser = new User({
+            name: req.body.name,
+            amount: req.body.amount
+            
+        })
+        const user = await newUser.save()
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
-          })
-        const savedUser = await newUser.save()
-          res.json(savedUser)
+router.put('/:id', async(req, res) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set:req.body
+        }, {new: true})
+        res.status(200).json(updatedUser)
+    } catch (err) {
+      res.send(500).json(err)  
+    }
+})
+router.delete('/:id',verifyTokenAndAuthorization, async(req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json('user has been deleted')
     } catch (err) {
         res.status(500).json(err)
-        }
-    })
+    }
+})
     module.exports = router

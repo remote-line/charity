40e116294588df2b0f311c867d5971d94e00b062
ChNafees5 <chnafees54@gmail.com>
2022-12-11 13:34:24 +0500

    update

--- apps/Server/routes/client.js
+++ apps/Server/routes/client.js
@@ -14,6 +14,24 @@ router.post('/register', async (req, res) => {
     }
 })
 
+router.get('/', async(req, res) => {
+    //const query = req.query.new
+    try {
+        const users = await User.find()
+       // const users = query ? await User.find().sort({_id:-1}).limits(5) : await User.find()
+        res.status(200).json(users)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+router.get('/find/:id', async(req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        res.status(200).json(user)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 router.put('/:id', async(req, res) => {
     try {
         const updatedUser = await User.findByIdAndUpdate(req.params.id, {
@@ -24,7 +42,7 @@ router.put('/:id', async(req, res) => {
       res.send(500).json(err)  
     }
 })
-router.delete('/:id',verifyTokenAndAuthorization, async(req, res) => {
+router.delete('/:id', async(req, res) => {
     try {
         await User.findByIdAndDelete(req.params.id)
         res.status(200).json('user has been deleted')

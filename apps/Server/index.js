d145bd63bab2ce76fd84c6c224d3959e50423fc1
ChNafees5 <chnafees54@gmail.com>
2022-12-25 23:18:08 +0500

    update the profile picture

--- apps/Server/index.js
+++ apps/Server/index.js
@@ -1,6 +1,8 @@
-const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 let port = 4000;
 var mongoDB = 'mongodb://127.0.0.1/charity';
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
@@ -14,6 +16,26 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => { console.log('Connected to Mongo database')});
 app.use(express.json())
+
+mongoose.Promise = global.Promise;
+app.use(morgan("dev"));
+app.use('/uploads', express.static('uploads'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
 const userRoute = require('./routes/User')
 const authRoute = require('./routes/auth')
  const clientRoute = require('./routes/client')
@@ -23,6 +45,20 @@ app.use('/api/auth', authRoute)
 app.use('/api/client', clientRoute)
 app.use('/api/profile', profileRoute)
 
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 app.listen(port, () => {
   console.log(`server is listening on Port: ${port}`);
 })
--- apps/Server/routes/User.js
+++ apps/Server/routes/User.js
@@ -1,10 +1,76 @@
 const router = require('express').Router()
-const User = require('../models/user')
+const Product = require('../models/user')
+const multer = require('multer');
+const mongoose = require("mongoose");
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const {authSchema}=require('../validation/usevalidation')
-//register
-router.post('/register', async (req, res) => {
+const upload1=multer({dest:'uploads/'});
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, new Date().toISOString().replace(/:/g, '-')+ file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  // reject a file
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
+});
+
+
+router.post("/register", upload.single('productImage'), async(req, res, next) => {
+ 
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(req.body.password, salt)
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    username: req.body.username,
+    email: req.body.email,
+    password: hashedPassword,
+    isAdmin: req.body.isAdmin,
+    productImage: req.file.path 
+  });
+  product
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "Created product successfully",
+        createdProduct: {
+            name: result.name,
+            price: result.price,
+            _id: result._id,
+            request: {
+                type: 'GET',
+                url: "http://localhost:4000/products/" + result._id
+            }
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+/*
+router.post('/register', upload.single('productImage'), async (req, res) => {
     try {
       //  console.log(req.body)
         const salt = await bcrypt.genSalt(10)
@@ -22,7 +88,7 @@ router.post('/register', async (req, res) => {
     } catch (error) {
         res.status(500).json({error:'user Name error '})
     }
-})
+})*/
  
 router.post('/login', async (req, res) => {
     try {
@@ -33,7 +99,7 @@ router.post('/login', async (req, res) => {
           res.status(400).send("All input is required");
         }
         // Validate if user exist in our database
-        const user = await User.findOne({ email });
+        const user = await Product.findOne({ email });
             console.log(user);
         if (user && await bcrypt.compare(password,user.password)) {
            
